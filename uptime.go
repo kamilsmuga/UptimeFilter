de16cb0a61dd41c2bb2b0d3a63fcebbd84a70d02
kamilsmuga <smugakamil@gmail.com>
2014-08-24 15:38:51 +0100

    declaration of FigureOutStartAndEndHour function

--- uptime.go
+++ uptime.go
@@ -30,7 +30,8 @@ func (f *UptimeFilter) Run(runner pipeline.FilterRunner, helper pipeline.PluginH
 	inChan := runner.InChan()
 	for pack = range inChan {
 		payload = pack.Message.GetPayload()
-		runner.LogMessage(payload)
+		runner.LogMessage("Payload: " + payload)
+
 		pack.Recycle()
 	}
 	return
@@ -40,4 +41,14 @@ func init() {
 	pipeline.RegisterPlugin("UptimeFilter", func() interface{} {
 		return new(UptimeFilter)
 	})
+}
+
+// UDP payloads with uptime can be delivered anytime. There's only a little chance
+// that this will occurr at e.g. 06:00
+// Need to parse epoch to figure out to which time period (between start and
+// end hour) epoch value belongs
+// Return start and end hour
+func (f *UptimeFilter) FigureOutStartAndEndHour(epoch int32) (startHour, endHour int32) {
+
+	return 1234, 5678
 }
