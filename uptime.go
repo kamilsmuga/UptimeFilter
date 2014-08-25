8b0d609a3bee3d7a4d2095301e1c17a3658c97f3
kamilsmuga <smugakamil@gmail.com>
2014-08-25 13:52:29 +0100

    a bit of refactoring

--- uptime.go
+++ uptime.go
@@ -42,12 +42,11 @@ func (f *UptimeFilter) Run(runner pipeline.FilterRunner, helper pipeline.PluginH
 		var epoch int64 = f.GetEpoch(payload)
 		f.startHour, f.endHour = f.FigureOutStartAndEndHour(epoch)
 		if !f.hours[f.startHour] {
-			f.initialUptime = f.GetUptime(payload)
-			f.lastUptime = f.initialUptime
-			f.totalUptime = f.initialUptime
-			f.dived = false
-			f.hours[f.startHour] = true
-			log.Printf("Stuff for hour: %d intialized", f.startHour)
+			f.InitFilterForStartHour(f.startHour, payload)
+		} else {
+			f.CalculateUptimeFor(f.startHour, f.endHour)
+			// f.hours[&f.startHour] = false
+			log.Printf("Length of map: &d", len(f.hours))
 		}
 		log.Printf("Start hour: %d", f.startHour)
 		log.Printf("End hour: %d", f.endHour)
@@ -57,10 +56,18 @@ func (f *UptimeFilter) Run(runner pipeline.FilterRunner, helper pipeline.PluginH
 	return
 }
 
-func init() {
-	pipeline.RegisterPlugin("UptimeFilter", func() interface{} {
-		return new(UptimeFilter)
-	})
+func (f *UptimeFilter) CalculateUptimeFor(startHour, endHour int64) {
+	// TODO
+	log.Printf("This function will calculate total uptime for period between: %d and %d", startHour, endHour)
+}
+
+func (f *UptimeFilter) InitFilterForStartHour(startHour int64, payload string) {
+	f.initialUptime = f.GetUptime(payload)
+	f.lastUptime = f.initialUptime
+	f.totalUptime = f.initialUptime
+	f.dived = false
+	f.hours[startHour] = true
+	log.Printf("Stuff for hour: %d initialized", &startHour)
 }
 
 func (f *UptimeFilter) GetEpoch(payload string) (epoch int64) {
@@ -100,4 +107,10 @@ func (f *UptimeFilter) FigureOutStartAndEndHour(epoch int64) (startHour, endHour
 	endEpoch := startEpoch.Add(time.Hour)
 	end := endEpoch.Hour()
 	return int64(start), int64(end)
+}
+
+func init() {
+	pipeline.RegisterPlugin("UptimeFilter", func() interface{} {
+		return new(UptimeFilter)
+	})
 }
